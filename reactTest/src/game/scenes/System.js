9b626682058f0fee5ffddcd33484f32c5f8f2b58
JackGartman <jackgartman04@vt.edu>
2025-11-04 18:09:34 -0500

    Implemented baseline version of Systems screen

--- reactTest/src/game/scenes/System.js
+++ reactTest/src/game/scenes/System.js
@@ -19,37 +19,276 @@ export default class System extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
-		// text_1
-		const text_1 = this.add.text(640, 360, "", {});
-		text_1.setInteractive(new Phaser.Geom.Rectangle(0, 0, 331, 45), Phaser.Geom.Rectangle.Contains);
-		text_1.setOrigin(0.5, 0.5);
-		text_1.text = "SYSTEM PAGE";
-		text_1.setStyle({ "color": "#818181ff", "fontSize": "50px", "resolution": 2 });
+		// Background
+		const background = this.add.rectangle(120, 80, 1040, 560);
+		background.setOrigin(0, 0);
+		background.isFilled = true;
+		background.fillColor = 29709;
+		background.isStroked = true;
+		background.strokeColor = 0;
 
-		this.text_1 = text_1;
+		// back_button
+		const back_button = this.add.ellipse(170, 130, 128, 128);
+		back_button.setInteractive(new Phaser.Geom.Rectangle(0, 0, 128, 128), Phaser.Geom.Rectangle.Contains);
+		back_button.scaleX = 0.5;
+		back_button.scaleY = 0.5;
+		back_button.isFilled = true;
+		back_button.isStroked = true;
+		back_button.strokeColor = 0;
+
+		// back_text
+		const back_text = this.add.text(151, 120, "", {});
+		back_text.text = "Back";
+		back_text.setStyle({ "color": "#000000ff" });
+
+		// stat_box
+		const stat_box = this.add.rectangle(150, 190, 500, 150);
+		stat_box.setOrigin(0, 0);
+		stat_box.isFilled = true;
+		stat_box.isStroked = true;
+		stat_box.strokeColor = 0;
+
+		// system_graph_box
+		const system_graph_box = this.add.rectangle(150, 363, 500, 190);
+		system_graph_box.setInteractive(new Phaser.Geom.Rectangle(0, 0, 500, 190), Phaser.Geom.Rectangle.Contains);
+		system_graph_box.setOrigin(0, 0);
+		system_graph_box.isFilled = true;
+		system_graph_box.isStroked = true;
+		system_graph_box.strokeColor = 0;
+
+		// week_button
+		const week_button = this.add.rectangle(674, 190, 145, 60);
+		week_button.setInteractive(new Phaser.Geom.Rectangle(0, 0, 145, 60), Phaser.Geom.Rectangle.Contains);
+		week_button.setOrigin(0, 0);
+		week_button.isFilled = true;
+		week_button.isStroked = true;
+		week_button.strokeColor = 0;
+
+		// month_button
+		const month_button = this.add.rectangle(829, 190, 145, 60);
+		month_button.setInteractive(new Phaser.Geom.Rectangle(0, 0, 145, 60), Phaser.Geom.Rectangle.Contains);
+		month_button.setOrigin(0, 0);
+		month_button.isFilled = true;
+		month_button.isStroked = true;
+		month_button.strokeColor = 0;
+
+		// year_button
+		const year_button = this.add.rectangle(984, 190, 145, 60);
+		year_button.setInteractive(new Phaser.Geom.Rectangle(0, 0, 145, 60), Phaser.Geom.Rectangle.Contains);
+		year_button.setOrigin(0, 0);
+		year_button.isFilled = true;
+		year_button.isStroked = true;
+		year_button.strokeColor = 0;
+
+		// production_graph_box
+		const production_graph_box = this.add.rectangle(674, 273, 455, 280);
+		production_graph_box.setInteractive(new Phaser.Geom.Rectangle(0, 0, 455, 280), Phaser.Geom.Rectangle.Contains);
+		production_graph_box.setOrigin(0, 0);
+		production_graph_box.isFilled = true;
+		production_graph_box.isStroked = true;
+		production_graph_box.strokeColor = 0;
+
+		// divider
+		const divider = this.add.rectangle(400, 265, 5, 128);
+		divider.isFilled = true;
+		divider.fillColor = 0;
+
+		// system_header
+		const system_header = this.add.text(400, 573, "", {});
+		system_header.setOrigin(0.5, 0);
+		system_header.text = "System";
+		system_header.setStyle({ "color": "#000000ff", "fontSize": "32px" });
+
+		// production_header
+		const production_header = this.add.text(901, 573, "", {});
+		production_header.setOrigin(0.5, 0);
+		production_header.text = "Production";
+		production_header.setStyle({ "color": "#000000ff", "fontSize": "32px" });
+
+		// money_value
+		const money_value = this.add.text(200, 228, "", {});
+		money_value.setOrigin(0, 0.5);
+		money_value.text = "[Money Value]";
+		money_value.setStyle({ "color": "#000000ff", "fontSize": "24px" });
+
+		// fish_amount
+		const fish_amount = this.add.text(200, 265, "", {});
+		fish_amount.setOrigin(0, 0.5);
+		fish_amount.text = "[Fish Amount]";
+		fish_amount.setStyle({ "color": "#000000ff", "fontSize": "24px" });
+
+		// plant_amount
+		const plant_amount = this.add.text(200, 302, "", {});
+		plant_amount.setOrigin(0, 0.5);
+		plant_amount.text = "[Plant Amount]";
+		plant_amount.setStyle({ "color": "#000000ff", "fontSize": "24px" });
+
+		// text_3
+		const text_3 = this.add.text(164, 220, "", {});
+		text_3.text = "[I]";
+		text_3.setStyle({ "color": "#000000ff" });
+
+		// text_4
+		const text_4 = this.add.text(162, 297, "", {});
+		text_4.text = "[I]";
+		text_4.setStyle({ "color": "#000000ff" });
+
+		// text_5
+		const text_5 = this.add.text(165, 256, "", {});
+		text_5.text = "[I]";
+		text_5.setStyle({ "color": "#000000ff" });
+
+		// energy_rate
+		const energy_rate = this.add.text(450, 240, "", {});
+		energy_rate.setOrigin(0, 0.5);
+		energy_rate.text = "[Energy Rate]";
+		energy_rate.setStyle({ "color": "#000000ff", "fontSize": "24px" });
+
+		// water_rate
+		const water_rate = this.add.text(450, 290, "", {});
+		water_rate.setOrigin(0, 0.5);
+		water_rate.text = "[Water Rate]";
+		water_rate.setStyle({ "color": "#000000ff", "fontSize": "24px" });
+
+		// text_6
+		const text_6 = this.add.text(412, 234, "", {});
+		text_6.text = "[I]";
+		text_6.setStyle({ "color": "#000000ff" });
+
+		// text_7
+		const text_7 = this.add.text(413, 280, "", {});
+		text_7.text = "[I]";
+		text_7.setStyle({ "color": "#000000ff" });
+
+		// week_text
+		const week_text = this.add.text(746, 220, "", {});
+		week_text.setOrigin(0.5, 0.5);
+		week_text.text = "Week";
+		week_text.setStyle({ "color": "#000000ff", "fontSize": "32px" });
+
+		// month_text
+		const month_text = this.add.text(901, 220, "", {});
+		month_text.setOrigin(0.5, 0.5);
+		month_text.text = "Month";
+		month_text.setStyle({ "color": "#000000ff", "fontSize": "32px" });
+
+		// year_text
+		const year_text = this.add.text(1056, 220, "", {});
+		year_text.setOrigin(0.5, 0.5);
+		year_text.text = "Year";
+		year_text.setStyle({ "color": "#000000ff", "fontSize": "32px" });
+
+		this.back_button = back_button;
+		this.system_graph_box = system_graph_box;
+		this.week_button = week_button;
+		this.month_button = month_button;
+		this.year_button = year_button;
+		this.production_graph_box = production_graph_box;
+		this.money_value = money_value;
+		this.fish_amount = fish_amount;
+		this.plant_amount = plant_amount;
+		this.energy_rate = energy_rate;
+		this.water_rate = water_rate;
 
 		this.events.emit("scene-awake");
 	}
 
+	/** @type {Phaser.GameObjects.Ellipse} */
+	back_button;
+	/** @type {Phaser.GameObjects.Rectangle} */
+	system_graph_box;
+	/** @type {Phaser.GameObjects.Rectangle} */
+	week_button;
+	/** @type {Phaser.GameObjects.Rectangle} */
+	month_button;
+	/** @type {Phaser.GameObjects.Rectangle} */
+	year_button;
+	/** @type {Phaser.GameObjects.Rectangle} */
+	production_graph_box;
 	/** @type {Phaser.GameObjects.Text} */
-	text_1;
+	money_value;
+	/** @type {Phaser.GameObjects.Text} */
+	fish_amount;
+	/** @type {Phaser.GameObjects.Text} */
+	plant_amount;
+	/** @type {Phaser.GameObjects.Text} */
+	energy_rate;
+	/** @type {Phaser.GameObjects.Text} */
+	water_rate;
 
-	/* START-USER-CODE */
+	/* FUNCTIONS */
 
-	// Write your code here
+	/**
+	 * This should get our graph of production, depending
+	 * on the setting that the user chose.
+	 */
+	getProdGraph(str) {
+		if (str == 'w') {
+			//Retrieve week graph
+		}
+		else if (str == 'm') {
+			//Retrieve month graph
+		}
+		else {
+			//Retrieve year graph
+		}
+	}
+
+	/**
+	 * This should get all the values needed for display
+	 * and retrieve our graph of energy and water.
+	 * 
+	 * It should also assign these values to the appropriate
+	 * text objects.
+	 */
+	populateSystemGraphAndValues() {
+		//Part 1: Retrieval - TODO
+		//Tests for now
+		this.money = 100;
+		this.fish = 0;
+		this.plants = 0;
+		this.water = 40;
+		this.energy = 30;
+		//Part 2: Assigning
+		this.money_value.text = this.money;
+		this.fish_amount.text = this.fish;
+		this.plant_amount.text = this.plants;
+		this.energy_rate.text = this.water;
+		this.water_rate.text = this.energy;
+	}
 
 	create() {
 
 		this.editorCreate();
 
-		this.text_1.on("pointerdown", () => {
-            this.scene.start('MainMenu'); // Still works!
+		this.populateSystemGraphAndValues();
+
+		this.back_button.on("pointerdown", () => {
+			// Stop this overlay scene
+            this.scene.stop();
+            // Resume the underlying main scene
+            this.scene.resume('MainMenu');
+		})
+
+		this.week_button.on("pointerdown", () => {
+            this.week_button.fillColor = 0x428544;
+			this.month_button.fillColor = 0xffffff;
+			this.year_button.fillColor = 0xffffff;
+			this.getProdGraph('w');
+        });
+
+		this.month_button.on("pointerdown", () => {
+            this.week_button.fillColor = 0xffffff;
+			this.month_button.fillColor = 0x428544;
+			this.year_button.fillColor = 0xffffff;
+			this.getProdGraph('m');
+        });
+
+		this.year_button.on("pointerdown", () => {
+            this.week_button.fillColor = 0xffffff;
+			this.month_button.fillColor = 0xffffff;
+			this.year_button.fillColor = 0x428544;
+			this.getProdGraph('y');
         });
 	}
-
-	/* END-USER-CODE */
 }
-
-/* END OF COMPILED CODE */
-
-// You can write more code here
