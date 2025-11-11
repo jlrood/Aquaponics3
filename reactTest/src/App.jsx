725897ee7404a31c226397e393c63a6ed757cae9
jlrood <jlrood@vt.edu>
2025-11-10 20:44:17 -0500

    fimsh

--- reactTest/src/App.jsx
+++ reactTest/src/App.jsx
@@ -48,13 +48,14 @@ function App ()
             const y = Phaser.Math.Between(64, scene.scale.height - 64);
 
             //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
-            const star = scene.add.sprite(x, y, 'star');
+            const juvenile = scene.add.sprite(x, y, 'juvenile');
+            juvenile.scale = 3;
 
             //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
             //  You could, of course, do this from within the Phaser Scene code, but this is just an example
             //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
             scene.add.tween({
-                targets: star,
+                targets: juvenile,
                 duration: 500 + Math.random() * 1000,
                 alpha: 0,
                 yoyo: true,
@@ -84,7 +85,7 @@ function App ()
                     <pre>{`{\n  x: ${spritePosition.x}\n  y: ${spritePosition.y}\n}`}</pre>
                 </div>
                 <div>
-                    <button className="button" onClick={addSprite}>Add New Sprite</button>
+                    <button className="button" onClick={addSprite}>Fimsh</button>
                 </div>
             </div>
         </div>
